583b93dd5bc07f60836448cb77d5b62d1f413b95
Six Jonathan <Six-jonathan@orange.fr>
2023-03-23 15:57:31 +0100

    add base archi

---
+++ Assets/Scripts/Physic/SmoothedParticleHydrodynamics.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SmoothedParticleHydrodynamics
+{
+    [SerializeField]
+    Vector2 m_offset;
+
+    Particle[] m_particles;
+
+    float GetWeight(Vector2 offset)
+    {
+        return 0;
+    }
+}
